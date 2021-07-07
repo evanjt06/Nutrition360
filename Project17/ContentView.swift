de8b6b9454cf3c453f567907d576830d1cc52fb5
evanjt06 <59384760+evanjt06@users.noreply.github.com>
2021-07-07 14:59:25 -0700

    Initial Commit

---
+++ Project17/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project17
+//
+//  Created by Evan Tu on 7/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project17/Project17App.swift
@@ -0,0 +1,17 @@
+//
+//  Project17App.swift
+//  Project17
+//
+//  Created by Evan Tu on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Project17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
