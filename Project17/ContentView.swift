98d8589a5131c87c592162d8636292114743e3af
evanjt06 <59384760+evanjt06@users.noreply.github.com>
2021-07-12 23:06:03 -0700

    vghjhk

--- Project17/ContentView.swift
+++ Project17/ContentView.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
+import ConcentricOnboarding
 
 struct ContentView: View {
     
+    @State private var hasSceneOnboardingScreen = false
     @State private var date = Date()
     
     var body: some View {
@@ -16,32 +18,54 @@ struct ContentView: View {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .long
         dateFormatter.timeStyle = .none
-         
-        
-        return TabView {
-            NavigationView {
-                MealsView()
-                    .navigationBarTitle("Meals - \(dateFormatter.string(from: date))")
-                    .navigationBarItems(trailing: DatePicker("", selection: $date, displayedComponents: .date))
-                    
-            }.tag(0)
-            .tabItem {
-                Image(systemName: "rectangle.split.3x3")
-                Text("Meals")
+
+        if !hasSceneOnboardingScreen {
+           
+            let pages = (0...3).map { i in
+                AnyView(PageView(title: MockData.title, imageName: MockData.imageNames[i], header: MockData.headers[i], content: MockData.contentStrings[i], textColor: MockData.textColors[i]))
             }
+
+            var a = ConcentricOnboardingView(pages: pages, bgColors: MockData.colors)
+
+    //        a.didPressNextButton = {
+    //            a.goToPreviousPage(animated: true)
+    //        }
+            a.insteadOfCyclingToFirstPage = {
+                print("do your thing")
+            }
+            a.animationDidEnd = {
+
+            }
+            a.didGoToLastPage = {
+            }
+            return AnyView(a)
             
-            
-            NavigationView {
-                ProgressView()
+        } else {
+            return AnyView(TabView {
+                NavigationView {
+                    MealsView()
+                        .navigationBarTitle("Meals - \(dateFormatter.string(from: date))")
+                        .navigationBarItems(trailing: DatePicker("", selection: $date, displayedComponents: .date))
+                        
+                }.tag(0)
+                .tabItem {
+                    Image(systemName: "rectangle.split.3x3")
+                    Text("Meals")
+                }
                 
-                    .navigationBarTitle("Progress")
-            }.tag(1)
-            .tabItem {
-                Image(systemName: "chart.bar.xaxis")
-                Text("Progress")
-            }
-            
-        }.accentColor(.red)
+                
+                NavigationView {
+                    ProgressView()
+                    
+                        .navigationBarTitle("Progress")
+                }.tag(1)
+                .tabItem {
+                    Image(systemName: "chart.bar.xaxis")
+                    Text("Progress")
+                }
+                
+            }.accentColor(.red))
+        }
     }
 }
 
@@ -50,3 +74,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
